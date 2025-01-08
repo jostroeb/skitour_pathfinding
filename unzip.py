3d044537d9962385e4d2a3492aa3a094ec5702f0
JohannesStroebel <stroebelj.tmt22@student.dhbw-heidenheim.de>
2025-01-08 17:09:37 +0100

    Add list return for extracted files; possibility to choose amount of files to be extracted;

--- unzip.py
+++ unzip.py
@@ -3,20 +3,36 @@ from os import listdir
 from os import path
 
 # function takes path and optional amount of files to be extracted starting from top, not looking at previously extracted files 
-def extractfiles(directory,amount = None): # insert directory using r'path' where r equals raw string literal
-    abspath = path.abspath(directory)
+# namereturn == True returns a list of all the processed files 
+def extractfiles(directory,amount = None,namereturn = None): 
+    directoryraw = r"{}".format(directory) # format the string as raw string
     i = 0
-    amount = amount - 1
-    for f in listdir(abspath): # for all files in this directory
+    namelist = []
+    if amount is not None:
+        amount = amount - 1
+    for f in listdir(directoryraw): # for all files in this directory
         name, ext = path.splitext(f)
         if ext == '.zip': # only use .zip files
-            if amount != None:
+            if amount is not None:
                 if i > amount: # counter for amount of files
-                    return(True)
+                    if namereturn is not None:
+                        return(namelist)
+                    else:
+                        return(True)
                 i = i + 1
-            fullpath = path.join(abspath,f) # create full path of zip file
+            fullpath = path.join(directoryraw,f) # create full path of zip file
             with zipfile.ZipFile(fullpath, 'r') as file: # r equals read only, with is exception handler
-                file.extractall(abspath) # extract zipfil
+                file.extractall(directoryraw) # extract zipfile
+                for fileending in file.namelist(): # for all files in extracted zipfile
+                    namelist.extend([path.join(directoryraw,fileending)]) # extend namelist with full path for each zipfile
+    print(namelist)
+    print("B")
+    if namereturn is not None:
+        return (namelist)
+
 
 if __name__ == "__main__":
-    extractfiles(r'C:\Users\ZOJSTROE\OneDrive - Carl Zeiss AG\Studium\T3100 - Studienarbeit\Karten\Karte_Garmisch',5) 
+    directory = input("Write directory without brackets e.g. [C:\Programs\Test]: ")
+    amount = int(input("Export first x files without checking if they already exist: "))
+    extracted = extractfiles(directory,amount,True) 
+    print(extracted)
